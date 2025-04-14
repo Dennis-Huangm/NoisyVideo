3bfb96895411e71c0f7eff96e9081951875b1228
ztt0821 <1320238991@qq.com>
2025-04-14 23:24:02 +0800

    add 2 temporal noise

--- demo.py
+++ demo.py
@@ -35,7 +35,7 @@ print(video_frames.shape)
 start = time.perf_counter()  # 性能计数器
 print(NoiseRegistry.list_noises())
 # video = NoiseRegistry.get_noise("color_quantized")(video_frames.clone(), 1)
-video = NoiseRegistry.get_noise("h265_artifacts")(video_frames.clone(), 1)
+video = NoiseRegistry.get_noise("elastic")(video_frames.clone(), 1)
 end = time.perf_counter()
 print(f"高精度计时: {end - start:.8f} 秒")
 save_image(video[0])
